9224f9a0595b7276d76a1a53306c160e1297f8de
Rishigesh <rishigesh720@gmail.com>
2026-09-08 17:33:18 +0530

    Fix missing closing parenthesis for RefreshIndicator in member_management_screen.dart

--- lib/features/members/screens/member_management_screen.dart
+++ lib/features/members/screens/member_management_screen.dart
@@ -321,6 +321,7 @@ class _MemberManagementScreenState extends State<MemberManagementScreen> with Si
                             );
                           },
                         ),
+                ),
               ],
             ),
     );
