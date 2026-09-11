1914ba8ca6b72ad5ccfbeda4845c526c365926c1
Rishigesh <rishigesh720@gmail.com>
2026-09-11 18:58:15 +0530

    deleteing all

--- android/app/src/main/kotlin/com/example/shopzo/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.shopzo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- shopzo_backend/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.ts'],
-  forceExit: true,
-  clearMocks: true,
-  resetMocks: true,
-  restoreMocks: true,
-};
